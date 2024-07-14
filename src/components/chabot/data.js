53c1158614bfa29b1eaa0eb2dac30ee2bea86316
joangao <64297106+Joan-gao@users.noreply.github.com>
2024-07-14 11:54:02 +0800

    chatbot ui update

---
+++ src/components/chabot/data.js
@@ -0,0 +1,103 @@
+export const initialMessages = [
+  {
+    type: 'system',
+    content: { text: 'Alibaba Cloud Intelligent Chatbot at your service' },
+  },
+  {
+    type: 'card',
+    content: {
+      image: '//gw.alicdn.com/tfs/TB1Xv5_vlr0gK0jSZFnXXbRRXXa-427-240.png',
+      title: 'Card title',
+      text: 'Card content',
+      button1Text: 'Default button',
+      button2Text: 'Primary button',
+    },
+  },
+];
+
+export const defaultQuickReplies = [
+  {
+    icon: 'message',
+    name: 'Contact Support',
+    isNew: true,
+    isHighlight: true,
+  },
+  {
+    name: 'Phrase 1',
+    isNew: true,
+  },
+  {
+    name: 'Phrase 2',
+    isHighlight: true,
+  },
+  {
+    name: 'Phrase 3',
+  },
+];
+
+export const cardMockData = {
+  "operations": [
+    {
+      "image": "https://gw.alicdn.com/tfs/TB19n9pg8fH8KJjy1XbXXbLdXXa-750-280.png",
+      "intro": "",
+      "link": "https://market.m.taobao.com/markets/xianyufamily/value-gaisir_copy?wh_weex=true",
+      "title": ""
+    },
+    {
+      "image": "https://gw.alicdn.com/tfs/TB1AJZuX8fH8KJjy1XbXXbLdXXa-682-214.png",
+      "intro": "",
+      "link": "https://h5.m.taobao.com/app/baqcenter/index.html?source=603#tradeIndemnity",
+      "title": ""
+    },
+    {
+      "image": "https://gw.alicdn.com/tfs/TB1jxjLSFXXXXcWXpXXXXXXXXXX-702-220.png",
+      "intro": "",
+      "link": "https://gw.alicdn.com/tfs/TB1hZDwSFXXXXbeXVXXXXXXXXXX-750-4310.png",
+      "title": ""
+    }
+  ],
+  "briefs": [
+    {
+      "title": "我的信用速卖交易问题",
+      "subContent": [
+        {
+          "title": "信用速卖订单在哪里？",
+          "content": "信用速卖订单在哪里？"
+        },
+        {
+          "title": "旧机型号找不到怎么办?",
+          "content": "信用速卖旧机型号找不到怎么办?"
+        },
+        {
+          "title": "我要如何填写发货单号？",
+          "content": "信用速卖如何填写单号？"
+        },
+        {
+          "title": "回收商质检退回后商品有问题怎么办？",
+          "content": "信用速卖质检退回后商品有问题怎么办？"
+        }
+      ]
+    },
+    {
+      "title": "什么是信用速卖？",
+      "subContent": [
+        {
+          "title": "信用速卖有哪些品牌可以参与？",
+          "content": "信用速卖涉及哪些品牌？"
+        },
+        {
+          "title": "信用速卖安全吗？",
+          "content": "信用速卖安全吗？"
+        },
+        {
+          "title": "信用速卖提示我未开通支付宝免密支付",
+          "content": "信用速卖提示我未开通支付宝免密支付"
+        },
+        {
+          "title": "信用速卖活动规则",
+          "content": "信用速卖活动规则"
+        }
+      ]
+    }
+  ]
+}
