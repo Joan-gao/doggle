f17d60544cbe3e5c70d709a95e8284adf6f298b8
amandaliberaann <amandaliberaann@gmail.com>
2024-07-17 09:13:21 +0800

    chatbot

---
+++ src/pages/Chatbot.js
@@ -0,0 +1,74 @@
+// App.jsx
+import React, { useEffect } from "react";
+import Chat, { Bubble, useMessages } from "@chatui/core";
+import "@chatui/core/dist/index.css";
+import "../assets/styles/chatui-theme.css";
+import CardComponent from "../components/chabot/CardComponent";
+
+import {
+  initialMessages,
+  defaultQuickReplies,
+} from "../components/chabot/data";
+
+const Chatbot = () => {
+  const { messages, appendMsg, setTyping } = useMessages(initialMessages);
+  // Inside Chatbot component
+
+  function handleSend(type, val) {
+    if (type === "text" && val.trim()) {
+      appendMsg({
+        type: "text",
+        content: { text: val },
+        position: "right",
+      });
+
+      setTyping(true);
+
+      setTimeout(() => {
+        appendMsg({
+          type: "text",
+          content: { text: "Hi I'm here! What can I help you?" },
+        });
+      }, 1000);
+    }
+  }
+
+  function handleQuickReplyClick(item) {
+    handleSend("text", item.name);
+  }
+
+  function renderMessageContent(msg) {
+    const { type, content } = msg;
+
+    switch (type) {
+      case "text":
+        return <Bubble content={content.text} />;
+      case "image":
+        return (
+          <Bubble type="image">
+            <img src={content.picUrl} alt="" />
+          </Bubble>
+        );
+      case "card":
+        return <CardComponent content={content} />;
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <div className="flex flex-row gap-2">
+      <Chat
+        navbar={{ title: "Finance AI Bot" }}
+        locale="en-US"
+        messages={messages}
+        renderMessageContent={renderMessageContent}
+        quickReplies={defaultQuickReplies}
+        onQuickReplyClick={handleQuickReplyClick}
+        onSend={handleSend}
+      />
+    </div>
+  );
+};
+
+export default Chatbot;
