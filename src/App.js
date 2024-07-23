cb3079ba624d025d22c4a7645fe41524a91cbee0
Joan-gao <xgao9917@gmail.com>
2024-07-23 08:05:20 +0800

    chatbot bug fixed

--- src/App.js
+++ src/App.js
@@ -16,13 +16,12 @@ import Billing from "./pages/Billing";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
-import Chatbot from './pages/Chatbot';
 import Main from "./components/layout/Main";
 import CalendarBill from "./pages/CalendarBill.js";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
-import ChatBotTest from "./pages/ChatBotTest.js";
+import ChatBot from "./pages/ChatBot.js";
 import Dashboard from "./pages/Dashboard.js"
 function App() {
   return (
@@ -36,10 +35,8 @@ function App() {
           <Route exact path="/tables" component={Tables} />
           <Route exact path="/billing" component={Billing} />
           <Route exact path="/profile" component={Profile} />
-          {/* <Route exact path="/chatbot" component={Chatbot} /> */}
-          <Route exact path="/chatbot" component={ChatBotTest} />
+          <Route exact path="/chatbot" component={ChatBot} />
           <Route exact path="/calendarbill" component={CalendarBill} />
-          {/* <Redirect from="*" to="/dashboard" /> */}
         </Main>
       </Switch>
     </div>
